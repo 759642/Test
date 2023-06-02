668bcfd96297f8610f9a5ce5ac040ecd1fa84da9
JackLi <jack.li@smartdaily.com.tw>
2023-06-02 17:08:37 +0800

    第一版
    第一版

---
+++ DistanceMatrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using GoogleMaps.DistanceMatrix;
+
+/*
+namespace DistanceMatrixExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Google Distance Matrix API 的金鑰
+            string apiKey = "Your_API_Key";
+
+            // 要查詢的起點地址
+            string origin = "New York, NY";
+
+            // 要查詢的終點地址列表
+            string[] destinations = { "Los Angeles, CA", "Chicago, IL", "Houston, TX" };
+
+            // 建立 DistanceMatrixService 物件
+            var service = new DistanceMatrixService(apiKey);
+
+            // 呼叫 API 進行距離和時間查詢
+            var result = service.GetDistanceMatrix(origin, destinations);
+
+            // 檢查 API 回傳的狀態
+            if (result.Status == DistanceMatrixStatusCodes.OK)
+            {
+                // 迭代處理每個終點地址的距離和時間
+                for (int i = 0; i < result.Rows.Count; i++)
+                {
+                    var distance = result.Rows[i].Elements[0].Distance.Text;
+                    var duration = result.Rows[i].Elements[0].Duration.Text;
+
+                    // 輸出結果
+                    Console.WriteLine("終點地址: " + destinations[i]);
+                    Console.WriteLine("距離： " + distance);
+                    Console.WriteLine("時間： " + duration);
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("距離和時間查詢失敗。狀態： " + result.Status);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+*/
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Newtonsoft.Json;
+using Quartz;
+using Quartz.Impl;
+using System;
+using Data = Google.Apis.Sheets.v4.Data;
+
+
+/*
+namespace SheetsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 設定 Google Sheets API 的認證憑證
+            GoogleCredential credential = GoogleCredential.FromFile("C:\\Users\\jackl\\source\\repos\\QuartzNetConsole\\oceanic-monitor-366608-02a88decbc73.json")
+                .CreateScoped(new[] { SheetsService.Scope.SpreadsheetsReadonly });
+
+            // 建立 SheetsService 物件
+            SheetsService service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Google Sheets Sample"
+            });
+
+            // 設定試算表的 ID
+            string spreadsheetId = "1mwBR_YSG6T4LAa87PoWgIqCAsixYsunwtM-R1uaVmjY";
+            // 設定要擷取的工作表名稱列表
+            List<string> sheetNames = new List<string> { "台北", "桃園", "新竹" };
+
+            // 迴圈處理每個工作表
+            foreach (string sheetName in sheetNames)
+            {
+                // 設定要擷取的範圍
+                string range = $"{sheetName}!A:D";
+
+                // 呼叫 Sheets API 取得資料
+                SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+                ValueRange response = request.Execute();
+
+                // 擷取到的資料
+                IList<IList<object>> values = response.Values;
+
+                // 輸出資料到控制台
+                if (values != null && values.Count > 0)
+                {
+                    Console.WriteLine($"工作表：{sheetName}");
+                    Console.WriteLine("擷取到的資料：");
+                    foreach (var row in values)
+                    {
+                        Console.WriteLine(string.Join(", ", row));
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"找不到資料。 工作表：{sheetName}");
+                }
+                Console.WriteLine();
+                Console.WriteLine(new string('=', 80));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+*/
+
+namespace SheetsSample
+{
+    public class FetchDataJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            // 設定 Google Sheets API 的認證憑證
+            GoogleCredential credential = GoogleCredential.FromFile("C:\\Users\\jackl\\source\\repos\\QuartzNetConsole\\oceanic-monitor-366608-02a88decbc73.json")
+                .CreateScoped(new[] { SheetsService.Scope.SpreadsheetsReadonly });
+
+            // 建立 SheetsService 物件
+            SheetsService service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Google Sheets Sample"
+            });
+
+            // 設定試算表的 ID
+            string spreadsheetId = "1mwBR_YSG6T4LAa87PoWgIqCAsixYsunwtM-R1uaVmjY";
+            // 設定要擷取的工作表名稱列表
+            List<string> sheetNames = new List<string> { "台北", "桃園", "新竹" };
+
+            // 迴圈處理每個工作表
+            foreach (string sheetName in sheetNames)
+            {
+                // 設定要擷取的範圍
+                string range = $"{sheetName}!A:D";
+
+                // 呼叫 Sheets API 取得資料
+                SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+                ValueRange response = request.Execute();
+
+                // 擷取到的資料
+                IList<IList<object>> values = response.Values;
+
+                // 輸出資料到控制台
+                if (values != null && values.Count > 0)
+                {
+                    Console.WriteLine($"工作表：{sheetName}");
+                    Console.WriteLine("擷取到的資料：");
+                    foreach (var row in values)
+                    {
+                        Console.WriteLine(string.Join(", ", row));
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"找不到資料。 工作表：{sheetName}");
+                }
+                Console.WriteLine();
+                Console.WriteLine(new string('=', 80));
+            }
+
+            Console.ReadLine();
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RunScheduler().GetAwaiter().GetResult();
+        }
+
+        static async Task RunScheduler()
+        {
+            // 配置 Quartz.Net 設定
+            var schedulerFactory = new StdSchedulerFactory();
+            var scheduler = await schedulerFactory.GetScheduler();
+            await scheduler.Start();
+
+            // 建立 Job
+            var job = JobBuilder.Create<FetchDataJob>()
+                .WithIdentity("fetchDataJob", "group1")
+                .Build();
+
+            // 建立 Trigger，每隔一分鐘執行一次
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("fetchDataTrigger", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x.WithIntervalInMinutes(1).RepeatForever())
+                .Build();
+
+            // 將 Job 和 Trigger 加入 Scheduler
+            await scheduler.ScheduleJob(job, trigger);
+
+            // 停止 Scheduler
+            await Task.Delay(TimeSpan.FromSeconds(60));
+            await scheduler.Shutdown();
+        }
+    }
+}
+
